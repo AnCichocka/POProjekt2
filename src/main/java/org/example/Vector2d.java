ba06d7ba2471bfc589260488c3ed9a3e764f8844
AnCichocka <annacichocka.poczta@gmail.com>
2023-01-29 21:27:53 +0100

    tag added

--- src/main/java/org/example/Vector2d.java
+++ src/main/java/org/example/Vector2d.java
@@ -10,24 +10,20 @@ public class Vector2d {
         this.x = x;
         this.y = y;
     }
-
     @Override
     public String toString(){
         return "(%d, %d)".formatted(x,y);
     }
-
     public boolean precedes(Vector2d other){
 
         return (this.x <= other.x && this.y <= other.y);
 
     }
-
     public boolean follows(Vector2d other){
 
         return (this.x >= other.x && this.y >= other.y);
 
     }
-
     public Vector2d add(Vector2d other){
 
         return new Vector2d(this.x + other.x, this.y + other.y);
